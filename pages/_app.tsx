00fc6f15deebaaa820eb87a4548f275184313d95
Josue A <josueroyale8171@gmail.com>
2023-02-03 17:02:37 +0000

    Chore: use vercel audience

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import "@/styles/globals.css";
 import "@/styles/markdown.css";
 import "codecrush-core/dist/index.css";
-import "@code-hike/mdx/dist/index.css"
+import "@code-hike/mdx/dist/index.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
